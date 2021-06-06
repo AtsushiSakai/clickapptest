60e52dc11cf05e7ac47f74e8dbd0d3ec8c2871ef
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-06-06 20:40:16 +0900

    change dir

--- clickapptest/__init__.py
+++ clickapptest/__init__.py
@@ -1,1 +1,6 @@
 
+# for common module use in other python scripts.
+try:  # for pip package
+    from clickapptest.common import common
+except ModuleNotFoundError:  # for local call
+    from common import common
